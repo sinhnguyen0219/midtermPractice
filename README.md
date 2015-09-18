e2f45b0675166a6fed1024e7888a249d442ad3ac
Sinh Nguyen <snguyen0219@gmail.com>
2015-09-18 15:37:09 -0500

    Initial Commit, passes the first acceptance test (All letters availible at the beginning)

---
+++ README.md
@@ -0,0 +1,20 @@
+CS 310 Midterm Exam Practice
+============================
+If this were a real exam, it would be worth 15 points. 
+It has 5 
+"questions", each represented by a unit test. 
+The first 3 questions have 
+their unit tests fully specified, 
+and you must implement the Hangman 
+class methods to ensure that those tests pass correctly. 
+The final 2 
+questions only have meaningful unit test names. 
+You must create the
+unit tests to satisfy the names and then implement the Hangman class 
+methods so that those tests pass correctly.
+
+
+If it is not clear yet, the goal of this code is to create a working
+Hangman game. 
+If you are not familiar with the game, visit
+https://en.wikipedia.org/wiki/Hangman_%28game%29
---
+++ src/main/java/edu/jsu/mcis/Hangman.java
@@ -0,0 +1,52 @@
+package edu.jsu.mcis;
+
+import java.util.*;
+
+public class Hangman {
+    public enum Result { WIN, LOSE, NONE };
+    private String word;
+    private List<Character> usedLetters;
+    
+    public Hangman() {
+        word = "";
+        usedLetters = new ArrayList<Character>();
+    }
+    
+    public void setWord(String word) {
+        this.word = word;
+    }
+    
+    public boolean available(char c) {
+		boolean avail = true;
+		for (int i = 0; i < usedLetters.size();i++){
+			if (c == usedLetters.get(i)){
+			avail = false;
+		}else{
+			avail = true;
+		}
+		}
+		return avail;
+	}
+    
+    
+    public int guess(char c) {
+        return 0;
+    }
+    
+    public Result getResult() {
+        return Result.NONE;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
